cf1b6a391542f895fb9fb3fe02fbe1ee3db31b07
SebasM79 <sebamussi.dev@hotmail.com>
2025-07-01 00:31:55 -0300

    modificacion a clase abstracta User y clases hijas passenger y driver

--- models/driver.py
+++ models/driver.py
@@ -1,6 +1,32 @@
 from models.user import User
 
 class Driver(User):
-    def __init__(self, name):
-        super().__init__(name)
-        self.available = True #Por ahora esta dispo
+    def __init__(self, name, dni, email, number_phone, patente):
+        super().__init__(name, dni, email, number_phone)
+        self._patente = patente
+        self._esta_trabajando = False
+
+    @property
+    def patente(self):
+        return self._patente
+
+    @patente.setter
+    def patente(self, valor):
+        self._patente = valor
+
+    @property
+    def esta_trabajando(self):
+        return self._esta_trabajando
+
+    #  modificar estel estado si se encuentra trabajando o no, 
+    
+    def definir_esta_trabajando(self, estado: bool):
+        self._esta_trabajando = estado
+
+    def calificar(self):
+        # hereda del metodo abstracto
+        print("Calificando chofer...")
+
+    def definir_zona_trabajo(self, zona: str):
+        # defino la zona donde se encuentra trabajando
+        print(f"Zona de trabajo establecida: {zona}")
--- models/passenger.py
+++ models/passenger.py
@@ -1,4 +1,18 @@
+# models/passenger.py
 from models.user import User
 
 class Passenger(User):
-    pass #Por ahora nada, pero  quuizas se podria agregar  "tipo pasajero" o algo asi
+    def __init__(self, name, dni, email, number_phone):
+        super().__init__(name, dni, email, number_phone)
+        self.direcciones = []
+
+    def calificar(self):
+        # Lógica para calificar al pasajero
+        print("Calificando pasajero...")
+
+    def agregar_direccion(self, direccion):
+        self.direcciones.append(direccion)
+
+    def definir_direccion_casa(self, direccion):
+        # Lógica para definir la dirección de casa
+        print(f"Dirección de casa establecida: {direccion}")
